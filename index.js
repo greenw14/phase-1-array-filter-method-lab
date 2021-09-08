6707358e22eeb0a2712cbd9158fa03d37fc7732a
william <willhgreen02@gmail.com>
2021-09-08 14:41:28 -0500

    commit tree 498071fecab9e408cc5014656178d6aa571aa1be

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return  drivers.filter(item => item == name || item == name.toLowerCase())
+}
+   
+function fuzzyMatch(drivers, firstLetter){
+    return  drivers.filter(item => item[0] === firstLetter[0])
+}
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+      return driver.name === name
+    })
+  }
